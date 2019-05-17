6b7d3ff825e443459a30aff484233ae6043d41da
GajananThenge <38664939+GajananThenge@users.noreply.github.com>
2019-05-17 12:29:11 +0530

    Update sql_use_case.py

--- sql_use_case.py
+++ sql_use_case.py
@@ -51,3 +51,40 @@ print(ps.sqldf(q2, locals()))
 
 
 
+
+
+##################################################################################################################################
+Gradient Descent
+
+Gradient Descent is most popular optimisation technique,used in machine learning and deep learning.
+It can be combined with almost every algorithm to minimize errors or losses.
+
+It is an optimization algorithm based on convex function that tweaks its parameters
+iteratively to minimize a given function to its local minimum.
+
+It simply calculate parameters or coefficient that minimize a cost function to its maximum extent.
+
+
+https://www.google.com/search?q=generalise+cost+function&safe=active&rlz=1C1GCEV_enIN846IN846&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi3qqPP66HiAhVytHEKHUSgCNQQ_AUIDigB&biw=1536&bih=674&dpr=1.25#imgrc=zgqwMH1c8jnwXM:
+https://towardsdatascience.com/step-by-step-guide-to-building-your-own-neural-network-from-scratch-df64b1c5ab6e
+https://towardsdatascience.com/gradient-descent-in-a-nutshell-eaf8c18212f0
+
+
+During forward propagation, a series of calculations is 
+performed to generate a prediction and to calculate the 
+cost. The cost is a function that we wish to minimize.
+In our case, the cost function will be: 
+
+https://www.google.com/search?q=generalise+cost+function&safe=active&rlz=1C1GCEV_enIN846IN846&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi3qqPP66HiAhVytHEKHUSgCNQQ_AUIDigB&biw=1536&bih=674&dpr=1.25#imgdii=_6HgD3Q7NQFflM:&imgrc=BOR0HhrNw26oWM:
+
+
+https://ml4a.github.io/ml4a/how_neural_networks_are_trained/
+
+**** https://hackernoon.com/gradient-descent-aynk-7cbe95a778da
+
+https://missinglink.ai/guides/neural-network-concepts/complete-guide-artificial-neural-networks/
+https://missinglink.ai/guides/neural-network-concepts/neural-network-bias-bias-neuron-overfitting-underfitting/
+
+
+
+
